f775ad600521d161b907a2571e349a881858cae7
zsiki <siki1958@gmail.com>
2025-09-02 12:34:14 +0200

    dialog translation bug fixed

--- i18n/hu.ts
+++ i18n/hu.ts
@@ -4,14 +4,29 @@
 <context>
     <name>ExportDialog</name>
     <message>
-        <location filename="../export_plugin_dialog.py" line="56"/>
-        <source>Missing file</source>
-        <translation>Hiányzó fájl</translation>
+        <location filename="../export_plugin_dialog_base.ui" line="14"/>
+        <source>GML export</source>
+        <translation>GML export</translation>
     </message>
     <message>
-        <location filename="../export_plugin_dialog.py" line="56"/>
-        <source>GeoPackage to be exported not found</source>
-        <translation>Exportálandó GeoPackage nem található</translation>
+        <location filename="../export_plugin_dialog_base.ui" line="22"/>
+        <source>GeoPackage file selection</source>
+        <translation>GeoPackage fájl kiválasztás</translation>
+    </message>
+    <message>
+        <location filename="../export_plugin_dialog_base.ui" line="32"/>
+        <source>GeoPackage to export:</source>
+        <translation>Exportálandó GeoPackage:</translation>
+    </message>
+    <message>
+        <location filename="../export_plugin_dialog_base.ui" line="39"/>
+        <source>Exportálás</source>
+        <translation>Exportálás</translation>
+    </message>
+    <message>
+        <location filename="../export_plugin_dialog_base.ui" line="52"/>
+        <source>Target GML:</source>
+        <translation>Eredmény GML:</translation>
     </message>
     <message>
         <location filename="../export_plugin_dialog.py" line="51"/>
@@ -22,6 +37,16 @@
         <location filename="../export_plugin_dialog.py" line="51"/>
         <source>Fill both fields</source>
         <translation>Töltse ki mindkét mezőt</translation>
+    </message>
+    <message>
+        <location filename="../export_plugin_dialog.py" line="56"/>
+        <source>Missing file</source>
+        <translation>Hiányzó fájl</translation>
+    </message>
+    <message>
+        <location filename="../export_plugin_dialog.py" line="56"/>
+        <source>GeoPackage to be exported not found</source>
+        <translation>Exportálandó GeoPackage nem található</translation>
     </message>
     <message>
         <location filename="../export_plugin_dialog.py" line="60"/>
@@ -35,26 +60,44 @@
     </message>
 </context>
 <context>
+    <name>GMLValidator</name>
+    <message>
+        <location filename="../validate_plugin_dialog_base.ui" line="14"/>
+        <source>GML validation</source>
+        <translation>GML ellenőrzés</translation>
+    </message>
+    <message>
+        <location filename="../validate_plugin_dialog_base.ui" line="22"/>
+        <source>GML:</source>
+        <translation>GML:</translation>
+    </message>
+    <message>
+        <location filename="../validate_plugin_dialog_base.ui" line="29"/>
+        <source>Open GML</source>
+        <translation>GML megnyitás</translation>
+    </message>
+</context>
+<context>
     <name>GmlExporter</name>
     <message>
         <location filename="../gml_exporter.py" line="75"/>
         <source>Unsupported geometry type: </source>
-        <translation>Nem kezelt geometria típus</translation>
+        <translation>Nem kezelt geometria típus:</translation>
     </message>
     <message>
         <location filename="../gml_exporter.py" line="152"/>
         <source>CRITICAL error</source>
-        <translation>KRITIKUS hiba</translation>
+        <translation>Végzetes hiba</translation>
     </message>
     <message>
         <location filename="../gml_exporter.py" line="152"/>
         <source>Cannot open GeoPackage: </source>
-        <translation>Nem lehet megnyitni a GeoPackage-t</translation>
+        <translation>Nem lehet megnyitni a GeoPackage fájlt:</translation>
     </message>
     <message>
         <location filename="../gml_exporter.py" line="188"/>
         <source>INFO</source>
-        <translation>INFO</translation>
+        <translation>Információ</translation>
     </message>
     <message>
         <location filename="../gml_exporter.py" line="188"/>
@@ -115,7 +158,7 @@
     <message>
         <location filename="../gml_importer.py" line="97"/>
         <source> layer </source>
-        <translation>réteg</translation>
+        <translation> réteg </translation>
     </message>
     <message>
         <location filename="../gml_importer.py" line="105"/>
@@ -125,17 +168,17 @@
     <message>
         <location filename="../gml_importer.py" line="105"/>
         <source> imported</source>
-        <translation>importálva</translation>
+        <translation> importálva</translation>
     </message>
     <message>
         <location filename="../gml_importer.py" line="115"/>
         <source>ERROR</source>
-        <translation>HIBA</translation>
+        <translation>Hiba</translation>
     </message>
     <message>
         <location filename="../gml_importer.py" line="115"/>
         <source> open/read error </source>
-        <translation>Nyitási/olvasási hiba</translation>
+        <translation> megnyitási/olvasási hiba </translation>
     </message>
 </context>
 <context>
@@ -143,7 +186,7 @@
     <message>
         <location filename="../gml_validator.py" line="52"/>
         <source>CRITICAL error</source>
-        <translation>KRITIKUS hiba</translation>
+        <translation>Végzetes hiba</translation>
     </message>
     <message>
         <location filename="../gml_validator.py" line="26"/>
@@ -153,41 +196,56 @@
     <message>
         <location filename="../gml_validator.py" line="39"/>
         <source>Schema not found: </source>
-        <translation>Séma nem található:</translation>
+        <translation>Séma nem található meg:</translation>
     </message>
     <message>
         <location filename="../gml_validator.py" line="45"/>
         <source>Error in schema: </source>
-        <translation>Hiba a sémában:</translation>
+        <translation>Hiba a sémában: </translation>
     </message>
     <message>
         <location filename="../gml_validator.py" line="52"/>
         <source>Invalid or missing GML file: </source>
-        <translation>Hibás vagy hiányzó GML fájl:</translation>
+        <translation>Hibás vagy hiányzó GML fájl: </translation>
     </message>
     <message>
         <location filename="../gml_validator.py" line="58"/>
         <source>ERROR</source>
-        <translation>HIBA</translation>
+        <translation>Hiba</translation>
     </message>
     <message>
         <location filename="../gml_validator.py" line="58"/>
         <source>Error in GML: </source>
-        <translation>Hiba a GML-ben:</translation>
+        <translation>Hiba a GML-ben: </translation>
     </message>
     <message>
         <location filename="../gml_validator.py" line="61"/>
         <source>INFO</source>
-        <translation>INFO</translation>
+        <translation>Információ</translation>
     </message>
     <message>
         <location filename="../gml_validator.py" line="61"/>
         <source> is valid to : </source>
-        <translation> megfelel ennek:</translation>
+        <translation> megfelel ennek: </translation>
     </message>
 </context>
 <context>
     <name>GpkgLoader</name>
+    <message>
+        <location filename="../load_plugin_dialog_base.ui" line="14"/>
+        <source>GeoPackage loader</source>
+        <translation>GeoPackage betöltés</translation>
+    </message>
+    <message>
+        <location filename="../load_plugin_dialog_base.ui" line="22"/>
+        <source>GeoPackage:</source>
+        <translation>GeoPackage:</translation>
+    </message>
+    <message>
+        <location filename="../load_plugin_dialog_base.ui" line="29"/>
+        <source>Open Geopackage</source>
+        <translation>Geopackage megnyitás</translation>
+    </message>
     <message>
         <location filename="../gpkg_loader.py" line="54"/>
         <source>Cannot load layer: </source>
@@ -196,6 +254,36 @@
 </context>
 <context>
     <name>ImportDialog</name>
+    <message>
+        <location filename="../import_plugin_dialog_base.ui" line="14"/>
+        <source>GML import</source>
+        <translation>GML import</translation>
+    </message>
+    <message>
+        <location filename="../import_plugin_dialog_base.ui" line="22"/>
+        <source>GML file selection</source>
+        <translation>GML fájl kiválasztás</translation>
+    </message>
+    <message>
+        <location filename="../import_plugin_dialog_base.ui" line="32"/>
+        <source>Target GeoPackage:</source>
+        <translation>Eredmény GeoPackage:</translation>
+    </message>
+    <message>
+        <location filename="../import_plugin_dialog_base.ui" line="39"/>
+        <source>Import</source>
+        <translation>Import</translation>
+    </message>
+    <message>
+        <location filename="../import_plugin_dialog_base.ui" line="52"/>
+        <source>Load layers</source>
+        <translation>Rétegek betöltése</translation>
+    </message>
+    <message>
+        <location filename="../import_plugin_dialog_base.ui" line="62"/>
+        <source>GML to import::</source>
+        <translation>Betöltendő GML:</translation>
+    </message>
     <message>
         <location filename="../import_plugin_dialog.py" line="47"/>
         <source>Empty field</source>
@@ -214,7 +302,7 @@
     <message>
         <location filename="../import_plugin_dialog.py" line="51"/>
         <source>GML file does not exist</source>
-        <translation>GML fájl nem létezik</translation>
+        <translation>GML fájl nem található</translation>
     </message>
     <message>
         <location filename="../import_plugin_dialog.py" line="55"/>
@@ -224,7 +312,17 @@
     <message>
         <location filename="../import_plugin_dialog.py" line="55"/>
         <source>GeoPackage already exists, overwrite?</source>
-        <translation>Geopackage fájl már létezik, felülírja?</translation>
+        <translation>GeoPackage fájl már létezik, felülírja?</translation>
+    </message>
+    <message>
+        <location filename="../xxx.py" line="59"/>
+        <source>GML fájl kiválasztása</source>
+        <translation>GML fájl kiválasztás</translation>
+    </message>
+    <message>
+        <location filename="../xxx.py" line="61"/>
+        <source>Importálás</source>
+        <translation>Importálás</translation>
     </message>
 </context>
 <context>
@@ -237,7 +335,7 @@
     <message>
         <location filename="../load_plugin_dialog.py" line="31"/>
         <source>GeoPackage field is obligatory</source>
-        <translation>GeoPackage mező kitöltése kötelező</translation>
+        <translation>Töltse ki a mezőt!</translation>
     </message>
 </context>
 <context>
@@ -253,12 +351,12 @@
     <message>
         <location filename="../xsd_structure.py" line="70"/>
         <source>Used E-Ing XSD structure: </source>
-        <translation>Használt E-Ing struktúra</translation>
+        <translation>Használ E-ING XSD struktúra: </translation>
     </message>
     <message>
         <location filename="../xsd_structure.py" line="77"/>
         <source>Supported E-Ing XSD version: </source>
-        <translation>Támogatott E-Ing XSD verzió</translation>
+        <translation>Támogatott E-ING XSD verzió: </translation>
     </message>
 </context>
 </TS>
